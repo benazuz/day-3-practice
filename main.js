6e632aa75c752bac06500362a98797f894b49202
Benazuz <azuz419@gmail.com>
2019-03-12 15:55:45 +0200

    starting the third part of the practice in arrays

---
+++ main.js
@@ -0,0 +1,29 @@
+// let companies=["Mazda", "Microsoft", "Apple", "Google"];
+// alert(companies);
+// console.log(companies.length);
+// let myFriends=[];
+// console.log(myFriends.length);
+// let infoMicrosoft=["Microsoft", 2019, true, "Computers"];
+// console.log(infoMicrosoft);
+// console.log(companies[5-1]);
+// let myStuff=["PC", "Bed", "Shoes"];
+// console.log(myStuff[3-2]);
+// let firstCompany=companies[0];
+// console.log(firstCompany);
+// let lastCompany=companies[companies.length-1];
+// console.log(lastCompany);
+// let lastIndex=companies.length - 1;
+// console.log(companies[lastIndex]);
+
+let plants=["Oak", "Lilly", "Belladona", "Tulip", "Yew"];
+console.log("The first plant is " + plants[0] + ", and the last one is " + plants[plants.length-1] + ".");
+plants[0]=plants[1];
+console.log("Now The first plant is "+plants[0]+", and the second one is still "+plants[plants.length-1]+".");
+plants=["Rose", "Banana", "Bush", "Tree", "Rock"];
+console.log(plants);
+console.log(plants[1]);
+plants.push("Coconut");
+console.log(plants);
+plants.push("Blueberries", "Blackberries");
+console.log(plants);
+
